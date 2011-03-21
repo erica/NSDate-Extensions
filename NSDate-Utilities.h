3f3b1c0f61d8c706c0ca2f225b6e48cabaec00df
emdentec <emdentec@gmail.com>
2011-03-21 16:54:29 +0000

    Changed Add / Subtract Methods to use NSInteger instead of NSUInteger

--- NSDate-Utilities.h
+++ NSDate-Utilities.h
@@ -41,12 +41,12 @@
 - (BOOL) isLaterThanDate: (NSDate *) aDate;
 
 // Adjusting dates
-- (NSDate *) dateByAddingDays: (NSUInteger) dDays;
-- (NSDate *) dateBySubtractingDays: (NSUInteger) dDays;
-- (NSDate *) dateByAddingHours: (NSUInteger) dHours;
-- (NSDate *) dateBySubtractingHours: (NSUInteger) dHours;
-- (NSDate *) dateByAddingMinutes: (NSUInteger) dMinutes;
-- (NSDate *) dateBySubtractingMinutes: (NSUInteger) dMinutes;
+- (NSDate *) dateByAddingDays: (NSInteger) dDays;
+- (NSDate *) dateBySubtractingDays: (NSInteger) dDays;
+- (NSDate *) dateByAddingHours: (NSInteger) dHours;
+- (NSDate *) dateBySubtractingHours: (NSInteger) dHours;
+- (NSDate *) dateByAddingMinutes: (NSInteger) dMinutes;
+- (NSDate *) dateBySubtractingMinutes: (NSInteger) dMinutes;
 - (NSDate *) dateAtStartOfDay;
 
 // Retrieving intervals
